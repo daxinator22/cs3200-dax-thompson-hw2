4a132084316b6d8e0590ec14268770c51cbf6fbc
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-02-24 20:46:13 -0700

    Counter view for watch created

--- TodoWatch/src/main/java/com/daxthompsontodoassignment2/watchapp/MainActivity.java
+++ TodoWatch/src/main/java/com/daxthompsontodoassignment2/watchapp/MainActivity.java
@@ -1,17 +1,45 @@
 package com.daxthompsontodoassignment2.watchapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.daxthompsontodoassignment2.api.Verify;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Verify.verifyWatchApp();
+
+        AppCompatTextView counter = findViewById(R.id.counter);
+        updateCounter(counter);
+
+
+        AppCompatButton plus = findViewById(R.id.plus);
+        plus.setOnClickListener((view -> {
+            this.counter++;
+            updateCounter(counter);
+        }));
+
+        AppCompatButton minus = findViewById(R.id.minus);
+        minus.setOnClickListener((view -> {
+            this.counter--;
+            updateCounter(counter);
+        }));
+
+    }
+
+    private void updateCounter(AppCompatTextView counter){
+        counter.setText(String.format("%d", this.counter));
     }
 }
